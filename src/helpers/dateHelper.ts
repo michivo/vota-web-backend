6f403a31e193b562c000bb5992d1afca812b4b1c
michivo <michivo@gmail.com>
2023-12-23 16:54:14 +0100

    improve error handling

--- src/helpers/dateHelper.ts
+++ src/helpers/dateHelper.ts
@@ -1,7 +1,12 @@
 export function toUtcDate(utcDateString: string): Date {
     // string should have the formate provided by sqlite, which is yyyy-mm-dd hh:mm:ss
-    if(utcDateString.match(/\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}/)) {
-        return new Date(`${utcDateString.replace(' ', 'T')}Z`);
+    try {
+        if (utcDateString.match(/\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}/)) {
+            return new Date(`${utcDateString.replace(' ', 'T')}Z`);
+        }
+        return new Date(utcDateString);
     }
-    return new Date(utcDateString);
+    catch {
+        return new Date();
+    }
 }
